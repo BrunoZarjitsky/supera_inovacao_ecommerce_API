b6a8b04e4d39fe49e4aa65fb89adb1b1a33af53a
brunozarjitsky <brunozar@poli.ufrj.br>
2023-02-10 14:25:24 -0300

    created routes to LoginView, LogoutView, RegisterView

--- setup/settings.py
+++ setup/settings.py
@@ -43,6 +43,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +136,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ACCOUNT_EMAIL_VERIFICATION = env('ACCOUNT_EMAIL_VERIFICATION')
--- setup/urls.py
+++ setup/urls.py
@@ -1,21 +1,31 @@
-"""setup URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import (
+    path, 
+    include, 
+)
+from rest_framework import routers
+from rest_auth.views import (
+    LoginView, 
+    LogoutView,
+)
+from rest_auth.registration.views import RegisterView
+
+from allauth.account.models import EmailAddress
+from allauth.socialaccount.models import SocialToken, SocialAccount, SocialApp
+from django.contrib.sites.models import Site
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('register/', RegisterView.as_view()),
+    path('', include(router.urls)),
 ]
+
+admin.site.unregister(SocialToken)
+admin.site.unregister(SocialAccount)
+admin.site.unregister(SocialApp)
+admin.site.unregister(EmailAddress)
+admin.site.unregister(Site)
